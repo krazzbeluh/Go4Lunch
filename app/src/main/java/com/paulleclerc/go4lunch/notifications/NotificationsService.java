f1ab91d941ab3ee6a1f012b163942f69b5e93610
Paul Leclerc <paulleclerc@icloud.com>
2020-06-16 14:43:40 +0200

    added notification management

---
+++ app/src/main/java/com/paulleclerc/go4lunch/notifications/NotificationsService.java
@@ -0,0 +1,87 @@
+/*
+ * NotificationsService.java
+ *   Go4Lunch
+ *
+ *   Updated by paulleclerc on 6/16/20 2:43 PM.
+ *   Copyright © 2020 Paul Leclerc. All rights reserved.
+ */
+
+package com.paulleclerc.go4lunch.notifications;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.paulleclerc.go4lunch.R;
+import com.paulleclerc.go4lunch.ui.login.LogInActivity;
+
+public class NotificationsService extends FirebaseMessagingService {
+    private static final int NOTIFICATION_ID = 801;
+    private static final String NOTIFICATION_TAG = "FIREBASEOC";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if (remoteMessage.getNotification() != null) {
+            String message = remoteMessage.getNotification().getBody();
+            // 8 - Show notification after received message
+            this.sendVisualNotification(message);
+        }
+    }
+
+    // ---
+
+    private void sendVisualNotification(String messageBody) {
+
+        // 1 - Create an Intent that will be shown when user will click on the Notification
+        Intent intent = new Intent(this, LogInActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        // 2 - Create a Style for the Notification
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        inboxStyle.setBigContentTitle(getString(R.string.app_name));
+        inboxStyle.addLine(messageBody);
+
+        // 3 - Create a Channel (Android 8)
+        String channelId = getString(R.string.default_notification_channel_id);
+
+        // 4 - Build a Notification object
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.go4lunch_logo)
+                        .setContentTitle(getString(R.string.app_name))
+                        .setContentText(getString(R.string.app_name))
+                        .setAutoCancel(true)
+                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                        //.setContentIntent(pendingIntent)
+                        .setStyle(inboxStyle);
+
+        // 5 - Add the Notification to the Notification Manager and show it.
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert notificationManager != null;
+
+        // 6 - Support Version >= Android 8
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "Message provenant de Firebase";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        // 7 - Show notification
+        notificationManager.notify(NOTIFICATION_TAG, NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+    }
+}
